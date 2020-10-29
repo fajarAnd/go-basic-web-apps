a45051deec4ed9bee97ebad8d5375317ef34dc7e
Fajar Andriansyah <fajar@myboost.id>
2020-10-29 14:48:20 +0700

    post json basic

--- main.go
+++ main.go
@@ -1,26 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"time"
 )
 
 type User struct {
-	FirstName string
-	LastName  string
-	Email     string
-	CreatedAt time.Time
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	CreatedAt time.Time `json:created_at`
 }
 type HomePageHandler struct {
 }
 
 func (h *HomePageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		name = "World"
-	}
-	fmt.Fprintf(w, "Hello %s!", name)
+	user := new(User)
+	json.NewDecoder(r.Body).Decode(user)
+	user.CreatedAt = time.Now()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	data, _ := json.Marshal(user)
+	w.Write(data)
 }
 
 func main() {
